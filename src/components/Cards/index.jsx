ec61940a33ab9f56953f9d309990c34bade34cb6
luh <lucineia.rsilvah@gmail.com>
2024-05-23 21:47:41 -0300

    tornando card dinamico

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,15 +1,15 @@
 import './Cards.css';
-function Card(){
-const img = 'https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+function Card({vol}){
+
   return(
   <>
    <div className='card'>
     <div className='foto'>
-    <img src={img} alt="foto"/>
+    <img src={vol.foto} alt="foto"/>
       
     </div>
-    <p>nome</p>
-    <p>desde: 2023</p>
+    <p>{vol.nome}</p>
+    <p>Voluntario desde: {vol.data}</p>
    </div>
   
   
--- src/components/Volunteers/index.jsx
+++ src/components/Volunteers/index.jsx
@@ -2,15 +2,35 @@ import Card from '../Cards';
 import './Volunteers.css';
 
 function Voluntarios(){
+  const voluntarios = [
+    {
+      id:'1',
+      foto:'https://images.unsplash.com/photo-1491349174775-aaafddd81942?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGVzc29hfGVufDB8fDB8fHwy',
+      nome: "ana",
+      data:"2022"
+    },
+    {
+      id:'2',
+      foto:'https://images.unsplash.com/photo-1554151228-14d9def656e4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGVzc29hfGVufDB8fDB8fHwy://unsplash.com/pt-br/fotografias/foto-em-tons-de-cinza-do-homem-c_GmwfHBDzk',
+      nome: "bia",
+      data:"2018"
+    },
+    {
+      id:'3',
+      foto:'https://images.unsplash.com/photo-1552058544-f2b08422138a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cGVzc29hfGVufDB8fDB8fHwy',
+      nome: "jose",
+      data:"2009"
+    },
+   
+  ];
   return(
     <>
     <section className='voluntarios'>
     <h2>Voluntarios</h2>
     <div className='lista'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      {voluntarios.map((vol)=>(
+        <Card key={vol.id} vol={vol}/>
+      ))}
 
 
     
